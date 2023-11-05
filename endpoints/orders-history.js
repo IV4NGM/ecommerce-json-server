c9561e3c388ddd0442dea598c5124adf13e61050
IvanGM <ivangm_01@hotmail.com>
2023-11-04 18:23:56 -0600

    feat: Modificados endpoints órdenes e historial

--- endpoints/orders-history.js
+++ endpoints/orders-history.js
@@ -3,23 +3,23 @@ const { validateToken } = require('../middlewares/jwt_validation')
 const { secret } = require('../config.js')
 
 router.get('/', validateToken(secret), (req, res) => {
-  const { email } = req.body
-  if (!email) {
+  const { id } = req.user
+  if (!id) {
     res.status(400).send({
-      message: 'A valid email must be provided'
+      message: 'A valid user ID must be provided'
     })
     return
   }
   const orders = router.db.getState().orders
 
   const ordersUser = orders.filter((order) => {
-    return order.email === email
+    return order.userId === id
   })
 
   if (ordersUser.length === 0) {
-    res.send({ message: 'There are NO orders' })
+    res.status(404).send({ message: 'There are NO orders' })
   } else {
-    res.send({ message: `There are ${ordersUser.length} orders`, data: ordersUser })
+    res.status(200).send({ message: `There are ${ordersUser.length} orders`, data: ordersUser })
   }
 })
 
--- endpoints/orders.js
+++ endpoints/orders.js
@@ -3,12 +3,16 @@ const { validateToken } = require('../middlewares/jwt_validation')
 const { secret } = require('../config.js')
 
 router.post('/', validateToken(secret), (req, res) => {
+  const { id } = req.user
+  const userId = id
   const orders = router.db.getState().orders
   const previousID = orders[0]?.orderId || 100
   const orderId = previousID + 1
+  const currentDate = new Date().toLocaleDateString()
+  const currentTime = new Date().toLocaleTimeString()
 
   const item = req.body
-  const order = { ...item, orderId }
+  const order = { ...item, orderId, userId, currentDate, currentTime }
   router.db.get('orders').unshift(order).write()
   res.send({ message: 'Order created successfully', data: order })
 })
